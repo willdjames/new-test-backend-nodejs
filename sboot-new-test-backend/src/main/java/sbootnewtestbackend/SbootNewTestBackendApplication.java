78dea4687e6a27afe9a6a4d82a5ab0a253bbb698
William Santana <williamdiegocampos@gmail.com>
2024-01-13 17:31:01 -0300

    Scaffold do componente

---
+++ sboot-new-test-backend/src/main/java/sbootnewtestbackend/SbootNewTestBackendApplication.java
@@ -0,0 +1,13 @@
+package sbootnewtestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootNewTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootNewTestBackendApplication.class, args);
+	}
+
+}
---
+++ sboot-new-test-backend/src/test/java/sbootnewtestbackend/SbootNewTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sbootnewtestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootNewTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
